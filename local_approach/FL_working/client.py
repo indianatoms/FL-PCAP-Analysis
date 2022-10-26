c87088876d45ced5c20f4e54e4bcff82976acfb2
indianatoms <ttomek.koziak@gmail.com>
2022-10-26 11:02:50 +0200

    Rearrange files

--- local_approach/FL_working/client.py
+++ local_approach/FL_working/client.py
@@ -25,10 +25,16 @@ class Client:
         self.feature_names = None
 
 
-    def load_data(self, path: str) -> pd.DataFrame:
+    def load_data(self, path: str, csids = False) -> pd.DataFrame:
         """Load csv representation of pcap data, which have 44 feature and one label where 1 indicates malicious communicaiton and 0 benign."""
 
-        dataset = pd.read_csv(path)
+        if csids:
+            hdrs = " Destination Port, Flow Duration, Total Fwd Packets, Total Backward Packets,Total Length of Fwd Packets, Total Length of Bwd Packets, Fwd Packet Length Max, Fwd Packet Length Min, Fwd Packet Length Mean, Fwd Packet Length Std,Bwd Packet Length Max, Bwd Packet Length Min, Bwd Packet Length Mean, Bwd Packet Length Std,Flow Bytes/s, Flow Packets/s, Flow IAT Mean, Flow IAT Std, Flow IAT Max, Flow IAT Min,Fwd IAT Total, Fwd IAT Mean, Fwd IAT Std, Fwd IAT Max, Fwd IAT Min,Bwd IAT Total, Bwd IAT Mean, Bwd IAT Std, Bwd IAT Max, Bwd IAT Min,Fwd PSH Flags, Bwd PSH Flags, Fwd URG Flags, Bwd URG Flags, Fwd Header Length, Bwd Header Length,Fwd Packets/s, Bwd Packets/s, Min Packet Length, Max Packet Length, Packet Length Mean, Packet Length Std, Packet Length Variance,FIN Flag Count, SYN Flag Count, RST Flag Count, PSH Flag Count, ACK Flag Count, URG Flag Count, CWE Flag Count, ECE Flag Count, Down/Up Ratio, Average Packet Size, Avg Fwd Segment Size, Avg Bwd Segment Size, Fwd Header Length2,Fwd Avg Bytes/Bulk, Fwd Avg Packets/Bulk, Fwd Avg Bulk Rate, Bwd Avg Bytes/Bulk, Bwd Avg Packets/Bulk,Bwd Avg Bulk Rate,Subflow Fwd Packets, Subflow Fwd Bytes, Subflow Bwd Packets, Subflow Bwd Bytes,Init_Win_bytes_forward, Init_Win_bytes_backward, act_data_pkt_fwd, min_seg_size_forward,Active Mean, Active Std, Active Max, Active Min,Idle Mean, Idle Std, Idle Max, Idle Min, Label"
+            columns = hdrs.split(",")
+            dataset = pd.read_csv(path,names=columns)
+        else:
+            dataset = pd.read_csv(path)
+
         return dataset
 
     def clean_dataset(self, df: pd.DataFrame):
@@ -107,7 +113,7 @@ class Client:
         
 
 
-    def init_empty_model(self,model_name, model):
+    def init_empty_model(self,model_name, model=None):
         if model_name == Supported_modles.SGD_classifier:
             self.model = SGDClassifier(n_jobs=-1, random_state=12, loss="log", learning_rate='optimal', eta0=0.15, verbose=0)
         if model_name == Supported_modles.MLP_classifier:
