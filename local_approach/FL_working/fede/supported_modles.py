86317a1a2eb09cb96bbcaa22a9dd07d2b5cd3a50
indianatoms <ttomek.koziak@gmail.com>
2022-11-30 13:03:39 +0100

    DOnt use ridge classifier

--- local_approach/FL_working/fede/supported_modles.py
+++ local_approach/FL_working/fede/supported_modles.py
@@ -5,6 +5,5 @@ class Supported_modles(str, Enum):
     logistic_regression = "LogisticRegression"
     SGD_classifier = "SGDClassifier"
     MLP_classifier = "MLP_classifier"
-    rigde_classifier = "RidgeClassifier"
     gradient_boosting_classifier = "GradientBoostingClassifier"
     NN_classifier = "NeuralNetworkClassifier"
